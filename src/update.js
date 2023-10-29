9f84436a21e38425c2664b1a9bc34910b662613b
José João Ferreira <jjasferreira@gmail.com>
2023-10-29 16:42:37 +0000

    Add final changes to `update.js` before devilery

--- src/update.js
+++ src/update.js
@@ -1,3 +1,68 @@
+function updateLineChart() {
+    var selectedYearlySales = [];
+
+    // Loop through the data to calculate the total sales for each year
+    d_vehicles.forEach(function(d) {
+    var year = parseInt(d.YEAR);
+    var units = +d.UNITS; // Convert units to a number
+    var yearEntry = selectedYearlySales.find(entry => entry.YEAR === year);
+
+    if (yearEntry && (selected_countries.includes(d.COUNTRY) || selected_countries.length == 0)) {
+        yearEntry.UNITS += units; // Add to existing total
+    } else if (selected_countries.includes(d.COUNTRY) || selected_countries.length == 0){
+        selectedYearlySales.push({ YEAR: year, UNITS: units }); // Initialize if it doesn't exist
+    }
+    });
+
+    const height = d3.select("#idiom1").node().getBoundingClientRect().height;
+    const width = d3.select("#idiom1").node().getBoundingClientRect().width;
+    const margin = {top: 20, right: 20, bottom: 25, left: 60};
+
+    console.log(selectedYearlySales);
+
+    const xScale = d3
+    .scalePoint()
+    .domain(selectedYearlySales.map(d => d.YEAR)) // Use map to extract years from the array of objects
+    .range([0, width - margin.left - margin.right])
+    .padding(0.1);
+    const yScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(selectedYearlySales, d => d.UNITS)]) // Use the maximum UNITS value
+    .range([height - margin.top - margin.bottom, 0]);
+
+    const line = d3.line()
+    .x((d) => xScale(d.YEAR))
+    .y((d) => yScale(d.UNITS));
+
+    const svg = d3.select("#idiom1").select("svg");
+
+    svg.select("path.line")
+    .datum(selectedYearlySales)
+    .transition()
+    .duration(500)
+    .attr("d", line);
+
+    // update x-axis
+    svg.select(".x-axis")
+    .transition()
+    .duration(500)
+    .call(d3.axisBottom(xScale));
+
+    // update y-axis
+    svg.select(".y-axis")
+    .transition()
+    .duration(500)
+    .call(d3.axisLeft(yScale).ticks(5));
+
+    // update circles
+    svg.selectAll(".circle.data")
+    .data(selectedYearlySales)
+    .transition()
+    .duration(500)
+    .attr("cx", d => xScale(d.YEAR))
+    .attr("cy", d => yScale(d.UNITS));
+}
+
 function updateChoropleth() {
 
     // Recompute data for selected countries
@@ -65,6 +130,23 @@ function update4() {
 
     // Selection changes
     {
+        const lines = document.querySelectorAll(".line.data");
+        lines.forEach((line) => {
+            if (selected_countries.length == 0) {
+                line.setAttribute("opacity", "0.65");
+                line.setAttribute("stroke-width", "3");
+                line.setAttribute("stroke", "url(#line-gradient)");
+            }
+            else if (!selected_countries.includes(line.getAttribute("country"))) {
+                line.setAttribute("opacity", "0.2");
+            }
+            else if (selected_countries.includes(line.getAttribute("country"))) {
+                line.setAttribute("opacity", "1");
+                line.setAttribute("stroke-width", "4");
+                line.setAttribute("stroke", "black");
+                line.parentNode.appendChild(line); // redraw them on top
+            }
+        });
         const circles_min = document.querySelectorAll(".circle-min.data");
         const circles_max = document.querySelectorAll(".circle-max.data");
         const circles = [...circles_min, ...circles_max];
@@ -73,11 +155,10 @@ function update4() {
                 circle.setAttribute("opacity", "1");
                 circle.setAttribute("stroke-width", "1")
             }
-            else if (!selected_countries.includes(circle.getAttribute("c"))) {
-                circle.setAttribute("opacity", "0.65");
-                circle.setAttribute("stroke-width", "1")
+            else if (!selected_countries.includes(circle.getAttribute("country"))) {
+                circle.setAttribute("opacity", "0.3");
             }
-            else if (selected_countries.includes(circle.getAttribute("c"))) {
+            else if (selected_countries.includes(circle.getAttribute("country"))) {
                 circle.setAttribute("opacity", "1");
                 circle.setAttribute("stroke-width", "2");
                 circle.parentNode.appendChild(circle); // redraw them on top
@@ -278,7 +359,6 @@ function update4() {
     }
 }
 
-
 function updateDotPlot() {
     const dots = document.querySelectorAll(".dot");
     dots.forEach((dot) => {
@@ -299,7 +379,7 @@ function updateDotPlot() {
 
     svg = d3.select("#idiom2").select("svg");
     const t = d3.transition().duration(1000); // Transition duration in milliseconds
-    const margin = { top: 40, right: 40, bottom: 40, left: 80 };
+    const margin = { top: 40, right: 40, bottom: 30, left: 80 };
     const container = d3.select("#idiom2");
     const height = container.node().clientHeight - margin.bottom - margin.top;
     const width = container.node().clientWidth - margin.left - margin.right;
@@ -394,4 +474,80 @@ function updateDotPlot() {
             .transition(t)
             .attr("cy", y);
     });
+}
+
+function update5() {
+    svg = d3.select("#idiom5").select("svg");
+    const prevMinYear = +svg.attr("minYear");
+    const prevMaxYear = +svg.attr("maxYear");
+    const prevCountries = svg.attr("countries").split(",");
+
+    if (minYear == prevMinYear && maxYear == prevMaxYear && selected_countries == prevCountries) {
+        return;
+    }
+    // If there have been changes, update the rectangles of heatmap
+    console.log(gd_vehicles);
+    const cd_vehicles = gd_vehicles.filter((d) => d.YEAR >= minYear && d.YEAR <= maxYear && (selected_countries.includes(d.COUNTRY) || selected_countries.length == 0));
+    console.log("cd_vehicles", cd_vehicles);
+    // Group data by vehicle and fuel type
+    const groupByVehicleFuel = d3.group(cd_vehicles, d => d.VEHICLE, d => d.FUEL);
+    // For each vehicle and fuel, sum the units
+    const vehicleFuelUnits = [];
+    groupByVehicleFuel.forEach((fuelMap, vehicle) => {
+        fuelMap.forEach((units, fuel) => {
+        vehicleFuelUnits.push({ vehicle, fuel, units: d3.sum(units, d => d.UNITS) });
+        });
+    });
+    // Compute total units sold across all vehicles and fuels
+    const totalUnits = d3.sum(vehicleFuelUnits, d => d.units);
+
+    const fuelUnits = {};
+    vehicleFuelUnits.forEach(d => {
+        if (!fuelUnits[d.fuel]) {
+        fuelUnits[d.fuel] = 0;
+        }
+        fuelUnits[d.fuel] += d.units;
+    });
+    const vehicleUnits = {};
+    vehicleFuelUnits.forEach(d => {
+        if (!vehicleUnits[d.vehicle]) {
+        vehicleUnits[d.vehicle] = 0;
+        }
+        vehicleUnits[d.vehicle] += d.units;
+    });
+    console.log("vehicleUnits", vehicleUnits);
+    console.log("fuelUnits", fuelUnits);
+
+    // update each square with transition of 500ms to new values (attr units, attr share attr share-vehicle attr share-fuel attr fill)
+    const rects = svg.selectAll(".rect.rect");
+    //change data
+    rects.data(vehicleFuelUnits).enter();
+    console.log(rects);
+    rects.each(function() {
+        const rect = d3.select(this);
+        /* console.log(rect);
+        const vehicle = rect.attr("vehicle");
+        const fuel = rect.attr("fuel");
+        const units = vehicleFuelUnits.find(d => d.vehicle === vehicle && d.fuel === fuel).units;
+        const share = units / totalUnits;
+        const shareVehicle = units / d3.sum(vehicleFuelUnits.filter(d => d.vehicle === vehicle), d => d.units);
+        const shareFuel = units / d3.sum(vehicleFuelUnits.filter(d => d.fuel === fuel), d => d.units); */
+        rect
+            .transition()
+            .duration(500)
+            .attr("units", d => d.units)
+            .attr("share", d => d.units / totalUnits)
+            .attr("share-vehicle", d => ((d.units/vehicleUnits[d.vehicle])*100).toFixed(1))
+            .attr("share-fuel", d => ((d.units/fuelUnits[d.fuel])*100).toFixed(1))
+            .attr("fill", d => {
+                // create color scale with domain going from 0 to max share for each fuel type
+                const colorScale = d3.scaleLinear()
+                  .domain([0, d3.max(vehicleFuelUnits.filter(o => o.fuel === d.fuel), d => d.units/fuelUnits[d.fuel])])
+                  .range([0, 1]);
+                console.log(d.units/fuelUnits[d.fuel], d.units, fuelUnits[d.fuel]);
+                const color = d3.interpolateGreens(colorScale(d.units/fuelUnits[d.fuel]));
+                return color;
+              });
+    });
+        
 }
